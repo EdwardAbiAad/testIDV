f078c3689c701e2890cb9775f2e63d36d6d86d92
Edward Abi Aad <eabiaad@areeba.com>
2025-06-25 09:24:29 +0300

    package fix

--- Package.swift
+++ Package.swift
@@ -11,24 +11,18 @@ let package = Package(
     products: [
         .library(
             name: "AreebaIDVSDK",
-            targets: ["IDWise","AreebaIDV"]
+            targets: ["AreebaIDVSDK","AreebaIDV"]
         )
     ],
     dependencies: [
         .package(url: "https://github.com/idwise/idwise-ios-sdk.git", from: "5.5.2")
     ],
     targets: [
+     
         .binaryTarget(
                    name: "AreebaIDV",
                    url: "https://www.dropbox.com/scl/fi/20fqhz4xr3etqlhcs5hl0/AreebaIDV.xcframework.zip?rlkey=yersh9s1pk046vsczpejo66th&st=itlkymiv&dl=1",
                    checksum: checksumForAreebaIDV
-               ),
-        .target(
-            name: "IDWise",
-            dependencies: [
-                .product(name: "IDWise", package: "idwise-ios-sdk")
-            ],
-            path: "Sources/AreebaIDVSDK"
-        )
+               )
     ]
 )
