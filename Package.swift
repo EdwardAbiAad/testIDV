e1b028213f823c1b6e6cae7e56e48d44ef34b5ca
Edward Abi Aad <eabiaad@areeba.com>
2025-06-25 09:32:32 +0300

    package fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "AreebaIDVSDK",
-            targets: ["AreebaIDVSDK","AreebaIDV"]
+            targets: ["AreebaIDV"]
         )
     ],
     dependencies: [
