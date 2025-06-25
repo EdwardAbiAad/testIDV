e1f3627f7f69022fda0dd035f2253107fb876a55
Edward Abi Aad <eabiaad@areeba.com>
2025-06-25 12:20:08 +0300

    new src code

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 6.1
 import PackageDescription
 
-let checksumForAreebaIDV = "da339a949c3f13f681dd7ef1a5312ed6758950f428133cbd823c33aedcba5814"
+let checksumForAreebaIDV = "275c79907536addda665310f222442dada8588545c3863c4650e09b940a79207"
 let checksumForShield = "28ed6c3a69c62e19fc508176cc4401e6e282a941b0110dfed85b528e39e4a4c3"
 let checksumForFP = "cdceb6f13d2302bfdbf95f26d013264c678058ffbf1226034aaa0be1763d796b"
 let checksumForIDWise = "b1be6e9dc98c641e6dc013ad4387ac4053fc76a010da415feb0a31fdbd0748f1"
@@ -19,7 +19,7 @@ let package = Package(
     products: [
         .library(
             name: "AreebaIDVSDK",
-            targets: [ "AreebaIDVTarget"]
+            targets: [ "AreebaIDVSDK"]
         )
     ],
     dependencies: [
@@ -28,17 +28,17 @@ let package = Package(
     targets: [
         .binaryTarget(
                    name: "AreebaIDV",
-                   url: "https://www.dropbox.com/scl/fi/20fqhz4xr3etqlhcs5hl0/AreebaIDV.xcframework.zip?rlkey=yersh9s1pk046vsczpejo66th&st=itlkymiv&dl=1",
+                   url: "https://www.dropbox.com/scl/fi/cgxnpyreal1j59gfa75u6/AreebaIDV.xcframework.zip?rlkey=62fdkw6mpxx2c3qco780jadbo&st=6je8p45s&dl=1",
                    checksum: checksumForAreebaIDV
                ),
         .target(
-                   name: "AreebaIDVTarget",
+                   name: "AreebaIDVSDK",
                    dependencies: [
                        "AreebaIDV", // Binary Target
                        .product(name: "IDWise", package: "idwise-ios-sdk")
                        
                    ],
-                   path: "Sources/AreebaIDVTarget"
+                   path: "Sources/AreebaIDVSDK"
                )
     ]
         
